7efbf913f9db88a8f8a21f4feda4b2cd476ea106
Sean Chen <seanschen1@gmail.com>
2022-03-12 14:24:53 -0800

    Added schedule message send every Sunday

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Time Scheduler for Discord
 Pairings are currently hardcoded, but bot can be extended to take in list of participants and output dynamic pairings based on week number
 
 Tutorial here: https://towardsaws.com/building-hosting-a-discord-bot-on-aws-e157bd7faf78
+
+## Commands
+
+| Plugin | README |
+| ------ | ------ |
+| !partners | prints out who the caller of this command is paired with this week |
+| !partners | prints out full schedule for this week |
+| !loop  | (Internal-only) starts task to print out schedule every Sunday  |
--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 # bot.py
-import os
-import datetime
-import json
+import os, datetime, json, asyncio
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,6 +9,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
+channel_id=943720617780334635
 
 pairs = [
     {
@@ -40,18 +39,7 @@ usernames = {
     "8384": "Kevin"
 }
 
-@bot.command(name='partners', help='Tells you who you are paired with this week 👍')
-async def get_pairs(ctx):
-
-    author = usernames[ctx.author.discriminator]
-    week_num = datetime.date.today().isocalendar()[1]
-    partners = pairs[week_num%len(pairs)][author]
-
-    await ctx.send('{}\'s partners this week are {} and {}'.format(author, partners[0], partners[1]))
-
-@bot.command(name='schedule', help='Full schedule for this week')
-async def get_schedule(ctx):
-
+def get_schedule():
     week_num = datetime.date.today().isocalendar()[1]
     partners = pairs[week_num%len(pairs)]
 
@@ -60,7 +48,38 @@ async def get_schedule(ctx):
     for key in partners:
         pretty_partners += '{}:\t{}, {}\n'.format(key, partners[key][0], partners[key][1])
 
+    return pretty_partners
 
-    await ctx.send('Schedule for this week:\n{}'.format(pretty_partners))
+# Sends schedule message out once a week (24 * 7)
+@tasks.loop(hours=168)
+async def send_weekly_schedule():
+    await bot.get_channel(channel_id).send(get_schedule())
+
+@send_weekly_schedule.before_loop
+async def before_send_weekly_schedule():
+    weekday_num = datetime.datetime.today().weekday()
+
+    # Sunday is 6
+    if(weekday_num != 0):
+        # Calculate number of seconds till Sunday and sleep before sending
+        seconds_till_sunday = (6-weekday_num)*24*60*60
+        await asyncio.sleep(seconds_till_sunday)
+
+@bot.command(name='partners', help='Tells you who you are paired with this week 👍')
+async def send_partners_message(ctx):
+
+    author = usernames[ctx.author.discriminator]
+    week_num = datetime.date.today().isocalendar()[1]
+    partners = pairs[week_num%len(pairs)][author]
+
+    await ctx.send('{}\'s partners this week are {} and {}'.format(author, partners[0], partners[1]))
+
+@bot.command(name='schedule', help='Full schedule for this week')
+async def send_schedule_message(ctx):
+    await ctx.send('Schedule for this week:\n{}'.format(get_schedule()))
+
+@bot.command(name='loop', help='Starts loops for bot')
+async def send_schedule_message(ctx):
+    send_weekly_schedule.start()
 
 bot.run(TOKEN)
